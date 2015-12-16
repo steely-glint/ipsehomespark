767f0324bc87f637b72d23cdd083681acb0da16b
Tim Panton <thp@westhawk.co.uk>
2015-12-16 11:59:16 +0000

    Added support for little serial TFT screen

--- src/main/java/com/ipseorama/ipsehomespark/App.java
+++ src/main/java/com/ipseorama/ipsehomespark/App.java
@@ -5,11 +5,15 @@ import com.phono.srtplight.Log;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
@@ -17,6 +21,7 @@ class App {
 
     private final Spark _spark;
     String _analogFile;
+    String _tty;
     static String EMH = "LightAlarm";
     Timer _tick;
 
@@ -33,10 +38,12 @@ class App {
         Log.debug("Spark " + _spark.toString());
 
         _analogFile = "/sys/devices/ocp.3/helper.15/AIN1";
+        _tty = "/dev/ttyO4";
         Log.debug("Using " + _analogFile);
         _tick = new Timer();
         TimerTask t = mkTask();
         _tick.schedule(t, 1, 1000);
+        setupScreen();
     }
 
     public void emergency(String volt) {
@@ -68,10 +75,10 @@ class App {
 
             // Post a text message to the room
             // Share a file with the room
-            message = new Message();
+            /*message = new Message();
             message.setRoomId(room.getId());
             message.setFiles(URI.create("https://upload.wikimedia.org/wikipedia/en/e/e1/TheDoctor.jpg"));
-            _spark.messages().post(message);
+            _spark.messages().post(message);*/
             message = new Message();
             message.setRoomId(room.getId());
             message.setText("Please state the nature of your emergency.");
@@ -91,6 +98,66 @@ class App {
     public static void main(String[] args) {
         App me = new App();
     }
+    /*
+    
+     Command
+     Function	Parameters	Notes
+     0	Clear Screen	none	 
+     1	Foreground Colour	Colour value (0-7)	 
+     2	Background Colour	Colour value (0-7)	 
+     3	Screen Rotation	Display Rotation value (0 - 3)	0 - Portrait (left)
+     1 - Landscape (upside down)
+     2 - Portrait (right)
+     3 - Landscape (default)
+     4	Font Size	Size Value (0 - 2)	1 - Small
+     2 - Medium (default)
+     3 - Large
+     5	Goto beginning of line	none	Based on current text size and position
+     6	Goto X, Y position (Text)	X, Y	Depends on currently selected font size for positioning
+     7	Goto X, Y position (Pixel)	X, Y	Pixel Positioning 
+     (X = 0 to 159, Y = 0 to 127) - Lanscape
+     (X = 0 to 127, Y = 0 to 159) - Portrait
+     0,0 is top left corner
+     8	Draw Line	X1, Y1, X2, Y2	Draws line in currently selected foreground colour between X1,Y1 and X2,Y2
+     9	Draw Box	X1, Y1, X2, Y2	Draws a box (outline) in currently selected foreground colour
+     10	Draw Filled Box	X1, Y1, X2, Y2	Draws a box (filled) in currently selected background colour
+     11	Draw Circle	X, Y, Radius	Draws a circle (outline) in currently selected foreground colour with centre at X, Y with radius R
+     12	Draw Filled Circle	X, Y, Radius	Draws a circle (filled) in currently selected background colour with centre at X, Y with radius R
+     13	Display Bitmap	X, Y, Filename	Display Bitmap (BMP) at coordinates X, Y
+     14	Backlight Brightness	0 - 100	Set backlight brightness (0 = OFF, 100 = Max).
+     */
+
+    public void writeScreen(byte[] bytes) {
+        try {
+            FileOutputStream tout = new FileOutputStream(_tty);
+            tout.write(bytes);
+            tout.close();
+        } catch (FileNotFoundException ex) {
+            Log.error("can't open tty " + _tty + " because" + ex);
+        } catch (IOException ex) {
+            Log.error("can't write to tty " + _tty + " because" + ex);
+        }
+    }
+
+    public void showVoltage(String v) {
+        while (v.length() < 9) {
+            v += " ";
+        }
+        byte[] vb = v.getBytes();
+        byte[] cls = {(byte) 0x1B, (byte) 6, (byte) 0, (byte) 2, (byte) 0xff};
+        byte[] message = new byte[cls.length + vb.length];
+        System.arraycopy(cls, 0, message, 0, cls.length);
+        System.arraycopy(vb, 0, message, cls.length, vb.length);
+        writeScreen(message);
+    }
+
+    public void setupScreen() {
+        byte[] setup = {(byte) 0x1B, (byte) 0, (byte) 0xff,
+            (byte) 0x1B, (byte) 14, (byte) 40, (byte) 0xff,
+            (byte) 0x1B, (byte) 4, (byte) 3, (byte) 0xff,
+            'W', 'e', 's', 't', 'h', 'a', 'w', 'k'};
+        writeScreen(setup);
+    }
 
     public String readVoltage() {
         String volt = "0";
@@ -101,8 +168,10 @@ class App {
             DataInputStream din = new DataInputStream(fin);
             volt = din.readLine();
             Log.debug("V = " + volt + "mv");
+            showVoltage(" " + volt + "mv");
         } catch (Exception ex) {
             Log.error("Problem with " + _analogFile + " : " + ex.getMessage());
+            ex.printStackTrace();
         } finally {
             try {
                 fin.close();
